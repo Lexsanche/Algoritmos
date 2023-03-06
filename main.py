a3d49e84d34fbdfe2996823cce6899aa08a9a4aa
JuanDR-19 <juanmanuelduranrueda@gmail.com>
2023-03-06 18:18:45 -0500

    adding the rest of the animations for the main samurai

--- main.py
+++ main.py
@@ -24,12 +24,49 @@ def main():
 
     #creando los loops de la animación de caminado
     animation_list = []
+    jump_animation = []
+    attack_animation_1 = []
+    attack_animation_2 = []
+    attack_animation_3 = []
+    dead_animation = []
+    hurt_animation = []
+    idle_animation = []
+    protection_animation = []
+    run_animation = []
     animation_steps = 10
     last_update = pygame.time.get_ticks()
     animation_cooldown = 75
     frame = 0
 
     main_samurai.animation_list(animation_steps, animation_list)
+    sprite_sheet = pygame.image.load('Samurai/Jump.png').convert_alpha()
+    main_samurai = samuraiMain.SamuraiSprite(sprite_sheet)
+    main_samurai.animation_list(animation_steps, jump_animation)
+    sprite_sheet = pygame.image.load('Samurai/Attack_1.png').convert_alpha()
+    main_samurai = samuraiMain.SamuraiSprite(sprite_sheet)
+    main_samurai.animation_list(animation_steps, attack_animation_1)
+    sprite_sheet = pygame.image.load('Samurai/Attack_2.png').convert_alpha()
+    main_samurai = samuraiMain.SamuraiSprite(sprite_sheet)
+    main_samurai.animation_list(animation_steps, attack_animation_2)
+    sprite_sheet = pygame.image.load('Samurai/Attack_3.png').convert_alpha()
+    main_samurai = samuraiMain.SamuraiSprite(sprite_sheet)
+    main_samurai.animation_list(animation_steps, attack_animation_3)
+    sprite_sheet = pygame.image.load('Samurai/Dead.png').convert_alpha()
+    main_samurai = samuraiMain.SamuraiSprite(sprite_sheet)
+    main_samurai.animation_list(animation_steps, dead_animation)
+    sprite_sheet = pygame.image.load('Samurai/Hurt.png').convert_alpha()
+    main_samurai = samuraiMain.SamuraiSprite(sprite_sheet)
+    main_samurai.animation_list(animation_steps, hurt_animation)
+    sprite_sheet = pygame.image.load('Samurai/Idle.png').convert_alpha()
+    main_samurai = samuraiMain.SamuraiSprite(sprite_sheet)
+    main_samurai.animation_list(animation_steps, idle_animation)
+    sprite_sheet = pygame.image.load('Samurai/Protection.png').convert_alpha()
+    main_samurai = samuraiMain.SamuraiSprite(sprite_sheet)
+    main_samurai.animation_list(animation_steps, protection_animation)
+    sprite_sheet = pygame.image.load('Samurai/Run.png').convert_alpha()
+    main_samurai = samuraiMain.SamuraiSprite(sprite_sheet)
+    main_samurai.animation_list(animation_steps, run_animation)
+
     # Definir el estado del juego
     game_over = False
 
